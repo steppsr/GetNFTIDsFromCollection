19e9cecb5256ab1ec84ad966ce86f2a471c9beea
Steve Stepp <steppsr@gmail.com>
2022-10-15 16:23:54 -0500

    Update to run as command
    Updating to run the script as a command. The collection ID is passed in as the first parameter. The output is now written to the screen and user should redirect the screen output to a file if needed. README.md update to reflect updates and example of how to run command.

--- README.md
+++ README.md
@@ -8,7 +8,14 @@ You will need your Mintgarden Collection ID and update that line in the script.
 1. Go to https://mintgarden.io
 2. Search for your collection name    _ie. BattleKats_
 3. URL result = https://mintgarden.io/collections/battlekats-col1kmrzafjx6ej8w79tz5vnjt4w8xuq2p6nmnheelgwwu3rsgsar0fsxc4wud
-4. Your Mintgarden Collection ID is everything after the hyphen in the resulting URL.  _ie. col1kmrzafjx6ej8w79tz5vnjt4w8xuq2p6nmnheelgwwu3rsgsar0fsxc4wud_
+4. Your Mintgarden Collection ID is everything after the hyphen in the resulting URL.  _ie. col1kmrzafjx6ej8w79tz5vnjt4w8xuq2p6nmnheelgwwu3rsgsar0fsxc4wud
 
-## Output
-The output is written into a file called `list.txt`
+## Running the command
+Use the `bash` command to run the script and pass in the Collection ID as the first parameter. You can also use redirect to send the output to a file.
+
+Example:
+```
+
+  bash nftids_fromcollection.sh col1kmrzafjx6ej8w79tz5vnjt4w8xuq2p6nmnheelgwwu3rsgsar0fsxc4wud > nftids.txt
+
+```
--- nftids.sh
+++ nftids.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-appdir=`pwd`
-cd ~/chia-blockchain
-. ./activate
-cd $appdir
-
 # How to find your Mintgarden Collection ID
 # 1. Go to https://mintgarden.io
 # 2. Search for your collection name    ie. BattleKats
 # 3. URL result = https://mintgarden.io/collections/battlekats-col1kmrzafjx6ej8w79tz5vnjt4w8xuq2p6nmnheelgwwu3rsgsar0fsxc4wud
 # 4. Your Mintgarden Collection ID is everything after the hyphen in the resulting URL.  ie. col1kmrzafjx6ej8w79tz5vnjt4w8xuq2p6nmnheelgwwu3rsgsar0fsxc4wud
-collection_id="col1kmrzafjx6ej8w79tz5vnjt4w8xuq2p6nmnheelgwwu3rsgsar0fsxc4wud"
-nfts=`curl -s https://api.mintgarden.io/collections/$collection_id/nfts/ids`
-echo $nfts | jq '.[].encoded_id' | cut -c 2- | rev | cut -c 2- | rev > ./list.txt
 
+# Running the command
+# Use the 'bash' command to run the script and pass the collection ID as the first parameter.
+# You can use redirect to send the output to a file.
+# Example:
+#   bash nftids_fromcollection.sh col1kmrzafjx6ej8w79tz5vnjt4w8xuq2p6nmnheelgwwu3rsgsar0fsxc4wud > nftids.txt
+
+collection_id=$1
+nfts=`curl -s https://api.mintgarden.io/collections/$collection_id/nfts/ids`
+echo $nfts | jq '.[].encoded_id' | cut -c 2- | rev | cut -c 2- | rev
